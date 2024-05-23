ad7c3977a8a632b8b0167242458d851b02b73b1a
Aniket gaikwad <100470741+Aniket17200@users.noreply.github.com>
2024-05-23 22:56:25 +0530

    first commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,6 +6,8 @@ export default function About() {
       <div className='py-40 bg-white text-center text-black'>
       <h2 className=' text-5xl lg:text-7xl  loading-snug font-bold mb-5'>About as Page</h2>
      </div>
+     {/* all blog container */}
+     <div className=' max-w-7xl max-auto'></div>
     </div>
   )
 }
